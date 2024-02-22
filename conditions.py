28353c76f9a80e03e2aefdc1c8bbf92ed8e3bd1a
Wangari-j <wangarijudy@gmail.com>
2024-02-22 05:45:01 +0300

    conditions

---
+++ conditions.py
@@ -0,0 +1,46 @@
+#if...elif..else statements
+#if..else executes two block of codes among two alternatives
+
+#we use if...elif...else to make a choice between more than two altenatives.
+
+number = 3
+
+if number > 3:
+    print('is greater')
+
+elif number<3:
+    print('is less than 3')
+
+else:
+    print('is equal to 3')
+
+#for many conditions
+    
+myCity = "Nanyuki"
+
+if myCity == "Nakuru":
+    print("My city is Nakuru")
+elif myCity == "Kisumu":
+    print("welcome to Dala")
+elif myCity == "Eldoret":
+    print("Welcome  to the home of champions")
+elif myCity == "Nanyuki":
+    print("The home of possibilities.")
+elif myCity == "Mombasa":
+    print("Mombasa raha")
+else:
+    print("Home away from home")
+
+
+person = "Joan"
+
+if person == "James":
+    print("My name is James.")
+elif person == "Jill":
+    print("My name is Jill.")
+elif person == "Jane":
+    print("MY name is Jane.")
+elif person == "Jonah":
+    print("My name  is Jonah")
+else:
+    print("I am Kenyan.")
